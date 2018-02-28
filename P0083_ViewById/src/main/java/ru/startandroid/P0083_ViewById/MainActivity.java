0fe66c8789dad864ee2b396784766e189e4b8bd4
Backtostack <backtostack@gmail.com>
2018-02-28 17:10:03 +0200

    P0082

---
+++ P0083_ViewById/src/main/java/ru/startandroid/P0083_ViewById/MainActivity.java
@@ -0,0 +1,12 @@
+package ru.startandroid.P0083_ViewById;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
