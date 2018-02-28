067a6a95ed091b57fabad3790f777e7cac4152fe
Backtostack <backtostack@gmail.com>
2018-02-28 17:31:32 +0200

    LastView

--- P0083_ViewById/src/main/java/ru/startandroid/P0083_ViewById/MainActivity.java
+++ P0083_ViewById/src/main/java/ru/startandroid/P0083_ViewById/MainActivity.java
@@ -1,12 +1,26 @@
 package ru.startandroid.P0083_ViewById;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+
+        TextView myTextView = (TextView) findViewById(R.id.myText);
+        myTextView.setText("New text in TextView");
+
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("My button");
+        myBtn.setEnabled(false);
+
+        CheckBox myChb = (CheckBox) findViewById(R.id.myChb);
+        myChb.setChecked(true);
     }
 }
