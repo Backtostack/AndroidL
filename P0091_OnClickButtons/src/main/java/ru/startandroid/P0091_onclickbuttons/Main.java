904bd731d3a20a461081cf332c3c08cf377e9cf4
Backtostack <backtostack@gmail.com>
2018-03-01 21:17:51 +0200

    Thursday_01.03.2018

--- P0091_OnClickButtons/src/main/java/ru/startandroid/P0091_onclickbuttons/Main.java
+++ P0091_OnClickButtons/src/main/java/ru/startandroid/P0091_onclickbuttons/Main.java
@@ -6,22 +6,18 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 
-public class Main extends AppCompatActivity {
-
+public class Main extends AppCompatActivity implements View.OnClickListener {
     TextView tvOut;
     Button btnOk;
     Button btnCancel;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
-
         //Найдем View элементы
         tvOut = (TextView) findViewById(R.id.tvOut);
         btnOk = (Button) findViewById(R.id.btnOk);
         btnCancel = (Button) findViewById(R.id.btnCancel);
-
         // создаем обработчик нажатия
         View.OnClickListener oclBtnOk = new View.OnClickListener() {
             @Override
@@ -29,19 +25,27 @@ public class Main extends AppCompatActivity {
                 // Меняем текст в TextView (tvOut)
                 tvOut.setText("Нажата кнопка ОК");
             }
-
         };
         // присвоим обработчик кнопке OK (btnOk)
         btnOk.setOnClickListener(oclBtnOk);
-        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Меняем текст в TextView (tvOut)
-                tvOut.setText("Нажата кнопка Cansel");
-            }
+        btnCancel.setOnClickListener(this);
 
-        };
+//        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Меняем текст в TextView (tvOut)
+//                tvOut.setText("Нажата кнопка Cansel");
+//            }
+//        };
         // присвоим обработчик кнопке OK (btnOk)
-        btnCancel.setOnClickListener(oclBtnCancel);
+//        btnCancel.setOnClickListener(oclBtnCancel);   s
+
+    }
+    @Override
+    public void onClick(View v) {
+        tvOut.setText("Нажата кнопка Cansel");
+    }
+    public void LessonClick (View view){
+        tvOut.setText("Нажата кнопка Lesson");
     }
 }
