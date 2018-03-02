f94240eebba07857f1fb764f21eefc85a49f674d
Backtostack <backtostack@gmail.com>
2018-03-02 14:10:05 +0200

    LastView

---
+++ P0101_Listener/src/main/java/ru/startandroid/P0101_listener/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.startandroid.P0101_listener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
