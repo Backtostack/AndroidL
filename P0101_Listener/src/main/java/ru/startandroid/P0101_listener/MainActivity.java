f57c031ad1d254f576cf44658868e45e8201570a
Backtostack <backtostack@gmail.com>
2018-03-04 19:54:05 +0200

    Sunday_04.03.2018

--- P0101_Listener/src/main/java/ru/startandroid/P0101_listener/MainActivity.java
+++ P0101_Listener/src/main/java/ru/startandroid/P0101_listener/MainActivity.java
@@ -2,6 +2,7 @@ package ru.startandroid.P0101_listener;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        //Найдем view - элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btnOk:
+                    tvOut.setText(R.string.btnOk);
+                    break;
+                case R.id.btnCancel:
+                    tvOut.setText(R.string.btnCan);
+                    break;
+            }
+            }
+        };
+        btnOk.setOnClickListener(onClickListener);
+        btnCancel.setOnClickListener(onClickListener);
+        tvOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnOk.setText(R.string.textButton);
+            }
+        });
     }
 }
---
+++ P0111_ResValues/src/main/java/ru/startandroid/P0111_resvalues/MainActivity.java
@@ -0,0 +1,23 @@
+package ru.startandroid.P0111_resvalues;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
+        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
+        Button btnBottom = (Button) findViewById(R.id.btnBottom);
+
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        tvBottom.setText(R.string.tvBottomText);
+        btnBottom.setText(R.string.btnBottomText);
+    }
+}
