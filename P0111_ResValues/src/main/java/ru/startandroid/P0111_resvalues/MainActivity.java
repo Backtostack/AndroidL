8c842a440ed66fe0394730c4803443be70523047
Backtostack <backtostack@gmail.com>
2018-03-06 17:08:34 +0200

    LastView

--- P0111_ResValues/src/main/java/ru/startandroid/P0111_resvalues/MainActivity.java
+++ P0111_ResValues/src/main/java/ru/startandroid/P0111_resvalues/MainActivity.java
@@ -2,22 +2,43 @@ package ru.startandroid.P0111_resvalues;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity  {
+    boolean flag = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+        //Найдем наши кнопки
         LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
         TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
         Button btnBottom = (Button) findViewById(R.id.btnBottom);
-
-        llBottom.setBackgroundResource(R.color.llBottomColor);
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        //Установим надпись кнопки
+        btnBottom.setText(R.string.Button);
         tvBottom.setText(R.string.tvBottomText);
-        btnBottom.setText(R.string.btnBottomText);
+        //Установим цвет кнопки
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        //Обработчик для смены картинки кнопки
+        btnBottom.setOnClickListener(new View.OnClickListener() {
+            //Обработчик нажатия кнопки
+            @Override
+            public void onClick(View v) {
+                if (flag)
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_camera_black_48dp));
+                else
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_add_a_photo_black_48dp));
+                flag=! flag;
+            }
+        });
     }
+
+
 }
