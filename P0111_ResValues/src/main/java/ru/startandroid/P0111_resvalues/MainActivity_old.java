c4e12f2bca2e3d96f4416a6f7e8491e3ecd8bcf7
Backtostack <backtostack@gmail.com>
2018-03-12 20:51:00 +0200

    Sunday_04.03.2018

--- P0111_ResValues/src/main/java/ru/startandroid/P0111_resvalues/MainActivity_old.java
+++ P0111_ResValues/src/main/java/ru/startandroid/P0111_resvalues/MainActivity_old.java
@@ -1,8 +1,7 @@
 package ru.startandroid.P0111_resvalues;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,32 +9,35 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView tvBottom;
-    ImageView imageView;
-    private static final String TAG = "myLogs";
+public class MainActivity_old extends AppCompatActivity  {
+    boolean flag = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
-        Log.d(TAG, "Найдем View элементы");
         //Найдем наши кнопки
         LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
         TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
         Button btnBottom = (Button) findViewById(R.id.btnBottom);
-        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView2);
         //Установим надпись кнопки
         btnBottom.setText(R.string.Button);
         tvBottom.setText(R.string.tvBottomText);
         //Установим цвет кнопки
         llBottom.setBackgroundResource(R.color.llBottomColor);
-        btnBottom.setOnClickListener(this);
         //Обработчик для смены картинки кнопки
+        btnBottom.setOnClickListener(new View.OnClickListener() {
+            //Обработчик нажатия кнопки
+            @Override
+            public void onClick(View v) {
+                if (flag)
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_camera_black_48dp));
+                else
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_add_a_photo_black_48dp));
+                flag=! flag;
+            }
+        });
     }
-    @Override
-    public void onClick(View v) {
-        tvBottom.setText("Нажата клавиша Смена картинки");
-        Log.d(TAG, "Проверим работу обработчика");
-    }
+
+
 }
