60d1ab6a238861a525f5bee9174a7337ecd2091a
Backtostack <backtostack@gmail.com>
2018-03-15 12:12:10 +0200

    15.03.18

--- P0131_MenuSimple/src/main/java/ru/startandroid/P0131_menusimple/MainActivity.java
+++ P0131_MenuSimple/src/main/java/ru/startandroid/P0131_menusimple/MainActivity.java
@@ -3,6 +3,8 @@ package ru.startandroid.P0131_menusimple;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -15,5 +17,47 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+    public boolean onOptionsItemSelected (MenuItem item){
+        /*Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();*/
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                Toast.makeText(this, getString(R.string.action_settings), Toast.LENGTH_SHORT).show();
+                break;
+             case R.id.action_item1:
+                 Toast.makeText(this, getString(R.string.action_item1), Toast.LENGTH_SHORT).show();
+                 break;
+            case R.id.action_item2:
+                 Toast.makeText(this, getString(R.string.action_item2), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_item3:
+                 Toast.makeText(this, getString(R.string.action_item3), Toast.LENGTH_SHORT).show();
+        }
+        /*if (id == R.id.action_settings){
+            Toast.makeText(this, getString(R.string.action_settings), Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.action_item1){
+            Toast.makeText(this, getString(R.string.action_item1), Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.action_item2){
+            Toast.makeText(this, getString(R.string.action_item2), Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.action_item3){
+            Toast.makeText(this, getString(R.string.action_item3), Toast.LENGTH_SHORT).show();
+        }*/
+            /*return true;*/
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+
+        menu.add("Settings");
+        menu.add("OpenVideo");
+        menu.add("OpenAudio");
+        menu.add("OpenPhoto");
+
+        return super.onCreateOptionsMenu(menu);
+    }*/
 }
 
