c1d837fa6bcc3ac571e468446bef29ecba1cad18
Backtostack <backtostack@gmail.com>
2018-03-15 12:56:57 +0200

    15.03.18

--- P0091_OnClickButtons/src/main/java/ru/startandroid/P0091_onclickbuttons/Main.java
+++ P0091_OnClickButtons/src/main/java/ru/startandroid/P0091_onclickbuttons/Main.java
@@ -7,9 +7,10 @@ import android.widget.TextView;
 import android.widget.Button;
 
 public class Main extends AppCompatActivity implements View.OnClickListener {
-    TextView tvOut;
-    Button btnOk;
-    Button btnCancel;
+    private TextView tvOut;
+    public Button btnOk;
+    public Button btnCancel;
+    public Button btnLesson;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,13 +19,17 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
         tvOut = (TextView) findViewById(R.id.tvOut);
         btnOk = (Button) findViewById(R.id.btnOk);
         btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnLesson = (Button) findViewById(R.id.btnLesson);
         // создаем обработчик нажатия
-        btnOk.setOnClickListener(new View.OnClickListener() {
+        /*btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tvOut.setText("Нажата кнопка ОК");
-            }
-        });
+            }*/
+        btnCancel.setOnClickListener(this);
+        btnOk.setOnClickListener(this);
+        btnLesson.setOnClickListener(this);
+        }
 //        View.OnClickListener oclBtnOk = new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -35,25 +40,31 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
         // присвоим обработчик кнопке OK (btnOk)
 //        btnOk.setOnClickListener(oclBtnOk);
 
-        btnCancel.setOnClickListener(this);
-    }
+        /*btnCancel.setOnClickListener(this);
+        btnOk.setOnClickListener(this);*/
+
 
 //        View.OnClickListener oclBtnCancel = new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
 //                // Меняем текст в TextView (tvOut)
-//                tvOut.setText("Нажата кнопка Cansel");
+//                tvOut.setText("Нажата кнопка Cancel");
 //            }
 //        };
         // присвоим обработчик кнопке OK (btnOk)
 //        btnCancel.setOnClickListener(oclBtnCancel);   s
-
-
     @Override
     public void onClick(View v) {
-        tvOut.setText("Нажата кнопка Cansel");
-    }
-    public void LessonClick (View view){
-        tvOut.setText("Нажата кнопка Lesson");
+        switch (v.getId()){
+            case R.id.btnOk:
+        tvOut.setText(R.string.OnOk);
+                break;
+            case R.id.btnCancel:
+        tvOut.setText(R.string.OnCansel);
+                break;
+            case R.id.btnLesson:
+        tvOut.setText(R.string.OnLesson);
+                break;
+        }
     }
 }
