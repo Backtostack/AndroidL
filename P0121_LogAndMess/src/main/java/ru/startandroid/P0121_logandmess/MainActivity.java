7631c41ab480a68c3ec41e84e929e517a2a73ebb
Backtostack <backtostack@gmail.com>
2018-03-19 21:06:25 +0200

    Sunday_04.03.2018

--- P0121_LogAndMess/src/main/java/ru/startandroid/P0121_logandmess/MainActivity.java
+++ P0121_LogAndMess/src/main/java/ru/startandroid/P0121_logandmess/MainActivity.java
@@ -3,22 +3,20 @@ package ru.startandroid.P0121_logandmess;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String TAG = "myLogs";
     TextView tvOut;
     Button btnOk;
     Button btnCancel;
-    ImageView imageView;
-
+    private static final String TAG = "myLogs";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,40 +26,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvOut = (TextView) findViewById(R.id.tvOut);
         btnOk = (Button) findViewById(R.id.btnOk);
         btnCancel = (Button) findViewById(R.id.btnCancel);
-        imageView = (ImageView) findViewById(R.id.imageView);
         // присваиваем обработчик кнопкам
         Log.d(TAG, "присваиваем обработчик кнопкам");
         btnOk.setOnClickListener(this);
         btnCancel.setOnClickListener(this);
-        //Установим картинку
-        Log.d(TAG, "устновим картинку");
-        imageView.setImageResource(R.drawable.ic_3d_rotation_black_36dp);
     }
+
     @Override
     public void onClick(View v) {
-        /*ImageView imageViewOk = new ImageView(MainActivity.this);
-        ImageView imageViewCancel = new ImageView(MainActivity.this);*/
         switch (v.getId()) {
             case R.id.btnOk:
                 // кнопка ОК
                 Log.d(TAG, "Нажата кнопка ОК");
                 tvOut.setText(R.string.НажатаКнопкаОК);
                 Toast.makeText(this, R.string.НажатаКнопкаОК, Toast.LENGTH_SHORT).show();
-                imageView.setImageResource(R.drawable.ic_accessibility_black_48dp);
                 break;
             case R.id.btnCancel:
                 // кнопка Cancel
                 Log.d(TAG, "Нажата нопка Cancel");
                 tvOut.setText(R.string.НажатаКнопкаCancel);
-                Toast.makeText(this, R.string.НажатаКнопкаCancel, Toast.LENGTH_SHORT).show();
-                imageView.setImageResource(R.drawable.ic_accessible_black_48dp);
-                /*Toast toast = Toast.makeText(this, R.string.НажатаКнопкаCancel, Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.CENTER,0,0);
+                Toast toast = Toast.makeText(this, R.string.НажатаКнопкаCancel, Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER,5,0);
                 LinearLayout toastImage = (LinearLayout) toast.getView();
                 ImageView imageView = new ImageView(MainActivity.this);
                 imageView.setImageResource(R.drawable.ic_3d_rotation_black_36dp);
                 toastImage.addView(imageView, 0);
-                toast.show();*/
+                toast.show();
                 break;
         }
 
