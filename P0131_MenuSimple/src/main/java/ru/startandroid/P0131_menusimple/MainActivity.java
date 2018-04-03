5325c21ea27ad7f2f985758d10ae89fd8ec026bc
Backtostack <backtostack@gmail.com>
2018-04-03 11:42:25 +0300

    03.04.2018

--- P0131_MenuSimple/src/main/java/ru/startandroid/P0131_menusimple/MainActivity.java
+++ P0131_MenuSimple/src/main/java/ru/startandroid/P0131_menusimple/MainActivity.java
@@ -4,26 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    CheckBox chb1, chb2;
+public class MainActivity extends AppCompatActivity  {
+    CheckBox chb1;
+    TextView textV;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        chb1 = (CheckBox) findViewById(R.id.chbExtGroup);
-        chb2 = (CheckBox) findViewById(R.id.chbExtItem);
+        chb1 = (CheckBox) findViewById(R.id.chbExtItem);
+        textV = (TextView) findViewById(R.id.textView);
+
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu, menu);
+        menu.add("First");
         return true;
     }
+
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.setGroupVisible(R.id.group1, chb1.isChecked());
 
         return super.onPrepareOptionsMenu(menu);
     }
@@ -33,15 +40,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_settings:
                 Toast.makeText(this, getString(R.string.action_settings), Toast.LENGTH_SHORT).show();
                 break;
-             case R.id.action_item1:
-                 Toast.makeText(this, getString(R.string.action_item1), Toast.LENGTH_SHORT).show();
-                 break;
-            case R.id.action_item2:
-                 Toast.makeText(this, getString(R.string.action_item2), Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.action_item3:
-                 Toast.makeText(this, getString(R.string.action_item3), Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
+
