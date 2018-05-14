bd4ef2e18b08bd7e487707c8721b4393d7ab2f2a
Backtostack <backtostack@gmail.com>
2018-05-14 22:28:34 +0300

    Monday_14.05.2018

---
+++ P0261_IntentFilter/src/main/java/ru/startandroid/P0261_intentfilter/TimeActivity.java
@@ -0,0 +1,13 @@
+package ru.startandroid.P0261_intentfilter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+    }
+}
