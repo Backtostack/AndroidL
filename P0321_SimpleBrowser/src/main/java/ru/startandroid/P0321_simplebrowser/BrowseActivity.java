300b50cc632530764ada9b3e86290a9263d9cdb8
Backtostack <backtostack@gmail.com>
2018-05-24 17:25:27 +0300

    24.05.2018

---
+++ P0321_SimpleBrowser/src/main/java/ru/startandroid/P0321_simplebrowser/BrowseActivity.java
@@ -0,0 +1,22 @@
+package ru.startandroid.P0321_simplebrowser;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class BrowseActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        Uri data = getIntent().getData();
+        webView.loadUrl(data.toString());
+    }
+}
--- P0321_SimpleBrowser/src/main/java/ru/startandroid/P0321_simplebrowser/MainActivity.java
+++ P0321_SimpleBrowser/src/main/java/ru/startandroid/P0321_simplebrowser/MainActivity.java
@@ -1,7 +1,10 @@
 package ru.startandroid.P0321_simplebrowser;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        (findViewById(R.id.btnWeb)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://fandroid.info")));
+            }
+        });
     }
 }
